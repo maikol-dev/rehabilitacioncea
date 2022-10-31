f118c2c6e21062bba22f3553ebc890a19891ddca
Maikol Castellano <56364360+Maik0x@users.noreply.github.com>
2022-10-31 17:49:18 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,34 +1,56 @@
-// navbar toggling
-const navbarShowBtn = document.querySelector('.navbar-show-btn');
-const navbarCollapseDiv = document.querySelector('.navbar-collapse');
-const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+// // navbar toggling
+// const navbarShowBtn = document.querySelector('.navbar-show-btn');
+// const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+// const navbarHideBtn = document.querySelector('.navbar-hide-btn');
 
-navbarShowBtn.addEventListener('click', function(){
-    navbarCollapseDiv.classList.add('navbar-show');
-});
-navbarHideBtn.addEventListener('click', function(){
-    navbarCollapseDiv.classList.remove('navbar-show');
-});
+// navbarShowBtn.addEventListener('click', function(){
+//     navbarCollapseDiv.classList.add('navbar-show');
+// });
+// navbarHideBtn.addEventListener('click', function(){
+//     navbarCollapseDiv.classList.remove('navbar-show');
+// });
 
-// changing search icon image on window resize
-window.addEventListener('resize', changeSearchIcon);
-function changeSearchIcon(){
-    let winSize = window.matchMedia("(min-width: 1200px)");
-    if(winSize.matches){
-        document.querySelector('.search-icon img').src = "images/search-icon.png";
-    } else {
-        document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
-    }
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const   navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
 }
-changeSearchIcon();
 
-// stopping all animation and transition
-let resizeTimer;
-window.addEventListener('resize', () =>{
-    document.body.classList.add('resize-animation-stopper');
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-        document.body.classList.remove('resize-animation-stopper');
-    }, 400);
-});
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+// // changing search icon image on window resize
+// window.addEventListener('resize', changeSearchIcon);
+// function changeSearchIcon(){
+//     let winSize = window.matchMedia("(min-width: 1200px)");
+//     if(winSize.matches){
+//         document.querySelector('.search-icon img').src = "images/search-icon.png";
+//     } else {
+//         document.querySelector('.search-icon img').src = "images/search-icon-dark.png";
+//     }
+// }
+// changeSearchIcon();
+
+// // stopping all animation and transition
+// let resizeTimer;
+// window.addEventListener('resize', () =>{
+//     document.body.classList.add('resize-animation-stopper');
+//     clearTimeout(resizeTimer);
+//     resizeTimer = setTimeout(() => {
+//         document.body.classList.remove('resize-animation-stopper');
+//     }, 400);
+// });
 
