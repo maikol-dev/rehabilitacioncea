cc48bca151e6549ab62efd23b1da81591aba0c6e
Maikol Castellano <56364360+Maik0x@users.noreply.github.com>
2022-11-03 23:57:55 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,7 +31,14 @@ if (navClose){
     })
 }
 
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 // // changing search icon image on window resize
 // window.addEventListener('resize', changeSearchIcon);
 // function changeSearchIcon(){
